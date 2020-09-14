cbb5f6238fd6c062d73a5798c422801058136430
cs187staff <dengyuntian@g.harvard.edu>
2020-09-14 16:35:28 +0000

    automatic update

--- tests/get_gold.py
+++ tests/get_gold.py
@@ -1,1 +1,3 @@
-test = {'name': 'get_gold', 'points': 1, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'get_gold',
+    'points': 1,
+    'suites': [{'cases': [{'code': ">>> testing[0]['authors'] == 'Madison'\nTrue", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- tests/majority_class.py
+++ tests/majority_class.py
@@ -1,1 +1,3 @@
-test = {'name': 'majority_class', 'points': 1, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {   'name': 'majority_class',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> type(majority_class(None)) == str\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
